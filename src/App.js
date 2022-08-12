b39fee9e5997f6e91d8f2317b0d1c50455c70600
Haja <hajarakotozafy45@gmail.com>
2022-08-12 22:12:32 +0200

    feat(avion): add pagination

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import { useEffect, useState } from 'react';
-import TableContainer from './Component/useTable';
 import axios from 'axios';
-import { TableHead, Table, TableBody, TableRow, TableCell } from '@material-ui/core';
+import { TableHead, Table, TableBody, TableRow, TableCell, makeStyles, TablePagination } from '@material-ui/core';
 
+const useStyles = makeStyles(theme => ({
+  table: {
+    marginTop: theme.spacing(3),
+    '& thead th': {
+      fontWeight: '600',
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.primary.light,
+    },
+    '& tbody td': {
+      fontWeight: '300',
+    },
+    '& tbody tr:hover': {
+      backgroundColor: '#fffbf2',
+      cursor: 'pointer',
+    },
+  },
+}))
 
 const App = () => {
   const [avions, setAvions] = useState([]);
+  const classes = useStyles();
+
+  const pages = [5, 10, 25];
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
 
   useEffect(async () => {
     await axios.get('http://localhost:3001/api/avion').then(res => {
@@ -13,9 +34,23 @@ const App = () => {
       setAvions(res.data);
     })
   }, [])
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  }
+
+  const handleChangeRowsPerPage = event => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  }
+
+  const recordsAfterPagingAndSorting = () => {
+    return avions.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
+  }
+
   return (
     <>
-      <Table>
+      <Table className={classes.table}>
         <TableHead>
           <TableRow>
             <TableCell>Désignation de l'avion</TableCell>
@@ -24,7 +59,7 @@ const App = () => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {avions.map(item => {
+          {recordsAfterPagingAndSorting().map(item => {
             return (
               <TableRow key={item.numAvion}>
                 <TableCell>{item.designation}</TableCell>
@@ -35,6 +70,15 @@ const App = () => {
           })}
         </TableBody>
       </Table>
+      <TablePagination
+        component="div"
+        page={page}
+        rowsPerPageOptions={pages}
+        rowsPerPage={rowsPerPage}
+        count={avions.length}
+        onChangePage={handleChangePage}
+        onChangeRowsPerPage={handleChangeRowsPerPage}
+      />
     </>
   )
 }
