7caaf561081518c3f178fab2389142ade0ee5d5f
Haja <hajarakotozafy45@gmail.com>
2022-08-13 11:43:02 +0200

    hotfix(avion): fix bug table

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { TableHead, Table, TableBody, TableRow, TableCell, makeStyles, TablePagination } from '@material-ui/core';
+import { TableHead, Table, TableBody, TableRow, TableCell, makeStyles, TablePagination, TableSortLabel } from '@material-ui/core';
 
 const useStyles = makeStyles(theme => ({
   table: {
@@ -27,6 +27,9 @@ const App = () => {
   const pages = [5, 10, 25];
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(pages[page]);
+  const [order, setOrder] = useState();
+  const [orderBy, setOrderBy] = useState();
+
 
   useEffect(async () => {
     await axios.get('http://localhost:3001/api/avion').then(res => {
@@ -48,14 +51,44 @@ const App = () => {
     return avions.slice(page * rowsPerPage, (page + 1) * rowsPerPage)
   }
 
+  const handleSortRequest = cellId => {
+    const isAsc = orderBy === cellId && order === "asc";
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(cellId);
+  }
+
   return (
     <>
       <Table className={classes.table}>
         <TableHead>
           <TableRow>
-            <TableCell>Désignation de l'avion</TableCell>
-            <TableCell>Nombre de place</TableCell>
-            <TableCell>Numéro de vol</TableCell>
+            <TableCell key="design">
+              <TableSortLabel
+                active={orderBy === "design"}
+                direction={orderBy === "design" ? order : 'asc'}
+                onClick={() => { handleSortRequest("design") }}
+              >
+                Désignation de l'avion
+              </TableSortLabel>
+            </TableCell>
+            <TableCell key="nbPlaces">
+              <TableSortLabel
+                active={orderBy === "nbPlaces"}
+                direction={orderBy === "nbPlaces" ? order : 'asc'}
+                onClick={() => { handleSortRequest("nbPlaces") }}
+              >
+                Nombre de place
+              </TableSortLabel>
+            </TableCell>
+            <TableCell key="numVol">
+              <TableSortLabel
+                active={orderBy === "numVol"}
+                direction={orderBy === "numVol" ? order : 'asc'}
+                onClick={() => { handleSortRequest("numVol") }}
+              >
+                Numéro de vol
+              </TableSortLabel>
+            </TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
